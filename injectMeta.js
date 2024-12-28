5cd24846abfff765104f3104b89c6cd9efc659df
mirbramo25 <193028411+mirbramo25@users.noreply.github.com>
2024-12-28 15:26:59 -0500

    up

---
+++ injectMeta.js
@@ -0,0 +1,8 @@
+const metaCharset = document.createElement('meta');
+metaCharset.setAttribute('charset', 'utf-8');
+document.head.appendChild(metaCharset);
+
+const metaViewport = document.createElement('meta');
+metaViewport.setAttribute('name', 'viewport');
+metaViewport.setAttribute('content', 'width=device-width, initial-scale=1.0');
+document.head.appendChild(metaViewport);
