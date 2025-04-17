1b359602afb76c45e68a403565c46d00f22bd86f
ASHELL MINI <mirbramo25@gmail.com>
2025-04-17 15:08:23 -0400

    commit from Push shortcut

--- functions/function_test.js
+++ functions/function_test.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
   return {
     statusCode: 200,
-    body: "Hellooooooo from my function!",
+    body: "Helloooooooooooooooooooooooooo from my function!",
   };
 };
